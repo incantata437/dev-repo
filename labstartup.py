f2e198042240234c4b0e0f1b9d234b14c85715b6
incantata437 <sangorilla@gmail.com>
2020-10-02 22:42:44 -0400

    Basic pseudo code and workflow defined - version 1

---
+++ labstartup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+import os
+import sys
+
+##mount the user1 share as user1
+
+
+
+##mount the user2 share as user2
+
+
+#begin test loop
+
+##user1
+##Begin writing user1 test data group
+##lots of small files
+
+##user2
+##Count/wait 10  Begin reading large file back
+
+
+##user1
+## Begin reading back user1 2nd batch with pauses
+
+
+##user3
+#mount SMB share for user3
+#copy data folder from host to Isilon
+#close SMB share
+
+
+##user1 
+##Delete target data on Isilon
+
+##user2 
+#remove locale large file
+
+##user3 
+#remove data folder on Isilon
+
+##repeat loop count of 10
