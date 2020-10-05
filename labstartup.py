bfa5204e1b7518d2af91da4bcea473623a9486b7
incantata437 <sangorilla@gmail.com>
2020-10-05 19:10:07 -0400

    Added beginning code for CIFS mount using subprocess lib and check_call

--- labstartup.py
+++ labstartup.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 import os
 import sys
+from subprocess import *
+
+#user1 = hwick@demo.local : Helen Wick
+#user2 = jwick@demo.local : John Wick
+#user3 = byaga@demo.local : Baba Yaga
+#user4 = patreides@demo.local : Paul Atreides
+#local ip = 192.168.1.15
+
+
 
 ##mount the user1 share as user1
 
@@ -10,6 +19,10 @@ import sys
 
 
 #begin test loop
+
+while loopcount < 15:
+    do something here
+
 
 ##user1
 ##Begin writing user1 test data group
@@ -25,6 +38,10 @@ import sys
 
 ##user3
 #mount SMB share for user3
+mountcifs = "mount -t cifs -o username=byaga@demo.local,password=Password123! //192.168.1.21/ifs/home/DEMO/byaga /mnt/byaga"
+subprocess.check_output(mountcifs)
+subprocess.check_call(mountcifs)
+
 #copy data folder from host to Isilon
 #close SMB share
 
