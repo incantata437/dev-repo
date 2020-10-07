096ebe305aaa70aa70edd3190ac7b0d025a30bfc
incantata437 <sangorilla@gmail.com>
2020-10-07 15:12:10 -0700

    added modules time and .... began adding SMB traffic loop

--- labstartup.py
+++ labstartup.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python
 import os
 import sys
-from subprocess import *
+import subprocess 
+import time
 
 #user1 = hwick@demo.local : Helen Wick
 #user2 = jwick@demo.local : John Wick
 #user3 = byaga@demo.local : Baba Yaga
 #user4 = patreides@demo.local : Paul Atreides
 #local ip = 192.168.1.15
-
-
+data_dir = "/usr/local/testdata3/"
+mountcmd = ["mount", 
+            "-t cifs", 
+            "-o username=byaga@demo.local,password=Password123!",
+            "/mnt/byaga"]
+targetdir = "/mnt/byaga"
 
 ##mount the user1 share as user1
 
-
-
 ##mount the user2 share as user2
-
 
 #begin test loop
 
@@ -31,20 +33,27 @@ while loopcount < 15:
 ##user2
 ##Count/wait 10  Begin reading large file back
 
-
 ##user1
 ## Begin reading back user1 2nd batch with pauses
 
-
 ##user3
 #mount SMB share for user3
-mountcifs = "mount -t cifs -o username=byaga@demo.local,password=Password123! //192.168.1.21/ifs/home/DEMO/byaga /mnt/byaga"
-subprocess.check_output(mountcifs)
-subprocess.check_call(mountcifs)
+#mountcifs = "mount -t cifs -o username=byaga@demo.local,password=Password123! //192.168.1.21/ifs/home/DEMO/byaga /mnt/byaga"
+
+os.chdir = (data_dir)
+subprocess.check_output(mountcmd)
+
+
 
 #copy data folder from host to Isilon
+for filename in os.listdir(data_dir):
+    subprocess.check_output("cp", filename, targetdir) 
+    time.sleep(20)
+    continue
+
 #close SMB share
 
+subprocess.check_output(["umount", "/mnt/byaga"])
 
 ##user1 
 ##Delete target data on Isilon
